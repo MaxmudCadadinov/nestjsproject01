afbef6f987ee9c65d3f4f18795e1b72a6fe8841f
mc <mc@mc.mc>
2025-07-13 10:23:41 +0500

    commit 13.07

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Body, Post, Delete, Param, Get, ParseIntPipe, HttpCode,UseInterceptors, UploadedFile} from '@nestjs/common';
+import { Controller, Body, Post, Delete, Param, Get, ParseIntPipe, HttpCode, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductService } from './product.service';
-import { CreateProductDto }  from './product.dto'; 
+import { CreateProductDto } from './product.dto';
 import { Product } from './product.entity';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
@@ -9,10 +9,10 @@ import { join } from 'path';
 
 @Controller()
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) { }
 
 
-  
+
 
   @Post('/add_product')
   @HttpCode(201)
@@ -26,33 +26,33 @@ export class ProductController {
       },
     })
   }))
-  async add_product( @Body() dto: CreateProductDto, @UploadedFile() file: Express.Multer.File) {
-    
-  const product = await this.productService.add_product(dto, file);
-  return product
-}
+  async add_product(@Body() dto: CreateProductDto, @UploadedFile() file: Express.Multer.File) {
+
+    const product = await this.productService.add_product(dto, file);
+    return product
+  }
 
   @Delete('/del_product/:id')
   @HttpCode(204)
   async del_product(@Param('id', ParseIntPipe) id: number) {
     return this.productService.del_product(id); // +id преобразует в число
-}
+  }
 
   @Delete('/decreased_products/:id/:num')
   @HttpCode(204)
   async decreased_product(@Param('id', ParseIntPipe) id: number, @Param('num', ParseIntPipe) num: number) {
     return this.productService.decreased_product(id, num)
-}
-
-
-  
-  @Get('/all_products/:page')
-  @HttpCode(200)
-  async all_products(@Param("page",ParseIntPipe) page:number): Promise<Product[]> {
-    return  await this.productService.find_all(page);
   }
 
 
-}   
+
+  @Get('/all_products/:page')
+  @HttpCode(200)
+  async all_products(@Param("page", ParseIntPipe) page: number): Promise<{ list: Product[], count: number }> {
+    return await this.productService.find_all(page);
+  }
+
+
+}
 
 
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -7,47 +7,49 @@ import { CreateProductDto } from './product.dto';
 
 @Injectable()
 export class ProductService {
-    constructor(
+  constructor(
     @InjectRepository(Product)
-  private productsRepo: Repository<Product>,
+    private productsRepo: Repository<Product>,
 
-){}
+  ) { }
 
-async add_product(dto: CreateProductDto, file: Express.Multer.File): Promise<Product>{
-    const new_product = await this.productsRepo.create({...dto, image:file.filename})
+  async add_product(dto: CreateProductDto, file: Express.Multer.File): Promise<Product> {
+    const new_product = await this.productsRepo.create({ ...dto, image: file.filename })
     return await this.productsRepo.save(new_product)
-    
-} 
 
-async del_product(id: number): Promise<void>{
+  }
+
+  async del_product(id: number): Promise<void> {
     const product = await this.productsRepo.findOneBy({ id: id })
-    if (!product){throw new Error('Product not found')}
+    if (!product) { throw new Error('Product not found') }
     await this.productsRepo.remove(product)
-    
+
+  }
+
+
+
+  async decreased_product(id: number, num: number): Promise<void> {
+    const product = await this.productsRepo.findOneBy({ id: id })
+    if (!product) { throw new Error('Product not found') }
+    if (product.quantity < num) { throw new Error('Not enough quantity') }
+    product.quantity -= num
+    await this.productsRepo.save(product)
+  }
+
+
+  async find_all(page: number) {
+    const limit = 10
+    const skip = (page - 1) * 10
+    const all_product_list = await this.productsRepo.find({ skip, take: limit })
+    const new_list = all_product_list.map(product => ({ ...product, image: `http://localhost:3000/images/${product.image}` }))
+    const count = await this.productsRepo.count()
+    console.log(count)
+    const count_page = Math.ceil(count / 10)
+    console.log(count_page)
+    return { "list": new_list, "count": count_page }
+  }
 }
-    
-
-
-async decreased_product(id: number, num: number): Promise<void> {
-  const product = await this.productsRepo.findOneBy({id: id})
-  if(!product){throw new Error('Product not found')}
-  if (product.quantity < num){throw new Error('Not enough quantity')}
-  product.quantity -= num
-  await this.productsRepo.save(product)
-}
-
-
-async find_all(page:number){
-  const limit=10
-  const skip=(page-1)*10
-  const all_product_list =await this.productsRepo.find({skip,take:limit})
-  const new_list = all_product_list.map(product => ({...product, image: `http://localhost:3000/images/${product.image}`})) 
-
-  return new_list
-}
- }
-    
 
 
 
-  
+
